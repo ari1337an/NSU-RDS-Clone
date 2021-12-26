97301c2a5766f1eecf7ed1fdb49227be0959ff75
Md Sahadul Hasan Arian <ari1337an@gmail.com>
2021-12-26 20:32:02 +0600

    Completed Faculty Course List

--- faculty/faculty_course_list.php
+++ faculty/faculty_course_list.php
@@ -29,7 +29,7 @@ if (!USERS::isLoggedFaculty()) {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../src/css/styles.css">
-    <title>Welcome | Admin Panel</title>
+    <title>Welcome | Faculty Panel</title>
 </head>
 
 <body>
@@ -37,8 +37,32 @@ if (!USERS::isLoggedFaculty()) {
     <?php include "../template/sub_header.php"; ?>
 
     <div class="container_panel">
-        
+
+    <table class="full_page_table">
+        <tr>
+            <th>Course ID</th>
+            <th>Course Name</th>
+        </tr>
+        <?php
+        $result = $APP_DB->query("
+SELECT t.course_id as 'course_id', c.course_name as 'course_name'
+FROM teaches as t JOIN course_list as c ON  t.course_id=c.course_id AND c.offer_status=1 AND t.who_is_teaching='" . USERS::getUserName() . "';
+");
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+        ?>
+                <tr>
+                    <td><?php echo $row['course_id']; ?></td>
+                    <td><?php echo $row['course_name'] ?></td>
+                </tr>
+        <?php
+            }
+        }
+        ?>
+    </table>
     </div>
+
+
 </body>
 
 </html>

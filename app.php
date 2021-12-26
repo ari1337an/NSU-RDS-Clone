44610856dea135798623eef1e699d5b3d4ee9e03
Md Sahadul Hasan Arian <ari1337an@gmail.com>
2021-12-26 18:44:49 +0600

    Reorganized the settings library for more general cases

--- app.php
+++ app.php
@@ -14,16 +14,11 @@ require PATH . 'configs/config.database.php';
 require PATH . './library/class.DB.php';
 require PATH . './library/class.Users.php';
 
-
 # Load the Libraries
 $APP_DB = new DB(new CONFIG_DB());
 
+# Settings Classess
 require PATH . './library/class.SETTINGS.php';
 
-# ReConstruct The Site Settings
-if(mysqli_num_rows($APP_DB->query("SELECT * FROM site_settings WHERE settings_name='advising_state'")) == 0){
-	$APP_DB->query("INSERT INTO site_settings(settings_name,settings_value) VALUE ('advising_state','0')"); // Default is Turned Off
-	// 0 = turned off
-	// 1 = turned on
-	// 2 = Ended
-}
+# Construct The Site Settings
+SETTINGS::init_settings("advising_state",0);  // Default is Turned Off ,0 = turned off, 1 = turned on, 2 = Ended
--- library/class.SETTINGS.php
+++ library/class.SETTINGS.php
@@ -2,20 +2,21 @@
    
 class SETTINGS{
 
-    public static function isAdvisingOn(){
-        
+    public static function init_settings($name, $default){
         global $APP_DB;
+        if(mysqli_num_rows($APP_DB->query("SELECT * FROM site_settings WHERE settings_name='$name'")) == 0){
+            $APP_DB->query("INSERT INTO site_settings(settings_name,settings_value) VALUE ('$name','$default')");
+        }
+    }
 
-        $sql = "SELECT * from site_settings";
-        $state = $APP_DB->query($sql);
-        
-        while($row = mysqli_fetch_assoc($state)){
-            if($row['settings_name'] == 'advising_state'){
+    public static function fetch_settings($name){
+        global $APP_DB;
+        $result = $APP_DB->query("SELECT * FROM site_settings");
+        while($row = mysqli_fetch_assoc($result)){
+            if($row['settings_name'] == $name){
                 return $row['settings_value'];
             }
         }
-        
-
     }
 
 }
